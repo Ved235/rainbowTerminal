2553df376802c471dea48fb4f7c69986d17654ff
Vedant Singhal <vedantsinghal07@gmail.com>
2025-08-10 21:46:37 -0400

    Initial commit

---
+++ tauri-terminal/src/main.ts
@@ -0,0 +1,66 @@
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import "xterm/css/xterm.css";
+import { invoke } from "@tauri-apps/api";
+
+const terminalElement = document.getElementById("terminal") as HTMLElement;
+
+const fitAddon = new FitAddon();
+const term = new Terminal({
+  fontFamily: "Jetbrains Mono",
+  allowTransparency: true,
+  cursorBlink: true,
+  cursorStyle: "bar",
+  theme: {
+    background: "transparent",
+    foreground: "#ff0000",
+  },
+});
+term.loadAddon(fitAddon);
+term.open(terminalElement);
+
+// Make the terminal fit all the window size
+async function fitTerminal() {
+  fitAddon.fit();
+  void invoke<string>("async_resize_pty", {
+    rows: term.rows,
+    cols: term.cols,
+  });
+}
+
+// Write data from pty into the terminal
+function writeToTerminal(data: string) {
+  return new Promise<void>((r) => {
+    term.write(data, () => r());
+  });
+}
+
+// Write data from the terminal to the pty
+function writeToPty(data: string) {
+  void invoke("async_write_to_pty", {
+    data,
+  });
+}
+function initShell() {
+  invoke("async_create_shell").catch((error) => {
+    // on linux it seem to to "Operation not permitted (os error 1)" but it still works because echo $SHELL give /bin/bash
+    console.error("Error creating shell:", error);
+  });
+}
+
+initShell();
+term.onData(writeToPty);
+addEventListener("resize", fitTerminal);
+fitTerminal();
+
+async function readFromPty() {
+  const data = await invoke<string>("async_read_from_pty");
+
+  if (data) {
+    await writeToTerminal(data);
+  }
+
+  window.requestAnimationFrame(readFromPty);
+}
+
+window.requestAnimationFrame(readFromPty);
